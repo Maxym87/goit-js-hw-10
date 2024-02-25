fba407739e7000e736412a728e895ee15743aa62
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-02-25 22:19:08 +0200

    add timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -36,24 +36,43 @@ const options = {
 flatpickr(input, options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
+
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
+
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
+
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+startBtn.addEventListener('click', () => {
+  let timerInterval = setInterval(() => {
+    let delta = new Date(input.value) - new Date();
+    startBtn.disabled = true;
+
+    if (delta >= 0) {
+      let timeObj = convertMs(delta);
+      days.textContent = addLeadingZero(timeObj.days);
+      hours.textContent = addLeadingZero(timeObj.hours);
+      minutes.textContent = addLeadingZero(timeObj.minutes);
+      seconds.textContent = addLeadingZero(timeObj.seconds);
+    } else {
+      clearInterval(timerInterval);
+      startBtn.disabled = false;
+      input.disabled = false;
+    }
+  }, 1000);
+  startBtn.disabled = true;
+  input.disabled = true;
+});
