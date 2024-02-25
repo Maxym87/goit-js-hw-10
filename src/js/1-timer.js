b592ac17f4e18547a2adb18d926fb566e8643485
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-02-25 20:31:21 +0200

    adds librarys

---
+++ src/js/1-timer.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const input = document.getElementById('datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const timer = document.querySelector('.timer');
+const daysValue = document.querySelector('span[data-days]');
+const hoursValue = document.querySelector('span[data-hours]');
+const minutesValue = document.querySelector('span[data-minutes]');
+const secondsValue = document.querySelector('span[data-seconds]');
