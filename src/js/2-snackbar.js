77f6ddffd85eb43642ad1ee96196b1a051133649
Maxym87 <112802810+Maxym87@users.noreply.github.com>
2024-02-28 16:09:52 +0200

    upd iziToast message

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,5 +1,6 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
+import successSvg from '../img/success.svg';
 
 const form = document.querySelector('.form');
 const input = document.querySelector('input');
@@ -26,13 +27,29 @@ form.addEventListener('submit', event => {
   createPromise(delay, state)
     .then(delay => {
       iziToast.success({
-        title: 'Success',
+        title: 'Ok',
+        titleColor: '#FFFFFF',
+        messageColor: '#FFF',
+        iconUrl: successSvg,
+        position: 'topRight',
+        backgroundColor: '#59A10D',
+        timeout: 3000,
+        progressBar: false,
+        close: false,
         message: `✅ Fulfilled promise in ${delay}ms`,
       });
     })
     .catch(delay => {
       iziToast.error({
         title: 'Error',
+        iconUrl: cross,
+        position: 'topRight',
+        timeout: 3000,
+        progressBar: false,
+        close: false,
+        titleColor: '#FFF',
+        messageColor: '#FFF',
+        backgroundColor: '#FF544B',
         message: `❌ Rejected promise in ${delay}ms`,
       });
     });
